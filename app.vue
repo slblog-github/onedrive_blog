4f9d80c689255e2d9ebbe834663608ec6cbbec55
arect <inspiration2030@outlook.com>
2021-12-12 13:07:11 +0800

    将网站名作为标题

--- app.vue
+++ app.vue
@@ -7,10 +7,12 @@
 <script>
 export default {
     setup () {
+        const config = useRuntimeConfig()
         useMeta({
             meta: [
                 { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }
             ],
+            title: config.WEBSITE_NAME
         })
     }
 }
