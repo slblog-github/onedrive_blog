522e8606ee11fdc2de269fdf22729ece5c0e6236
Hajeekn <64446074+slblog-github@users.noreply.github.com>
2022-04-16 21:19:50 +0800

    Update loading.vue

--- components/loading.vue
+++ components/loading.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex fixed top-0 left-0 z-20 w-screen h-screen justify-center items-center backdrop-filter backdrop-blur">
         <div>
-            <img src="~/assets/onedrive.png" alt="logo" class="filter -hue-rotate-45 animate-pulse">
+            <div class="loading"></div>
         </div>
     </div>
 </template>
